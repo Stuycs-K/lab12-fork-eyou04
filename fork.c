76b8593979ee3e6f9201b1957da45daaafc6c362
Eric You <lyou50@stuy.edu>
2024-11-26 11:18:14 -0500

    got the forks working and printing

--- fork.c
+++ fork.c
@@ -32,48 +32,60 @@ int random_number() {
 }
 
 int main() {
+    int child_1_num = random_number();
+    int child_2_num = random_number();
     printf("%d about to create 2 child processes\n", getpid());
-    pid_t child_1 = fork();
+    pid_t child_1, child_2;
+    child_1 = fork();
     if (child_1 < 0){
         perror("child_1 fork fail\n");
         exit(1);
     }
     if (child_1 == 0) {
-        int rand_num = random_number();
-        printf("%d %dsec\n", getpid() ,rand_num);
-        sleep(rand_num);
-        //printf("%d finished after %d seconds\n",getpid(), rand_num);
+        printf("%d %dsec\n", getpid() , child_1_num);
+        sleep(child_1_num);
+        printf("%d finished after %d seconds\n",getpid(), child_1_num);
+    } else {
+        int status;
+        pid_t child_pid = wait(&status);
+
+        if (child_pid == -1) {
+            perror("child_1 wait failed\n");
+            exit(1);
+        }
+        
+        if (!(WIFEXITED(status))) {
+            printf("%d child_2 did not exit normally\n", getpid());
+        }
+        
+        printf("Main Process %d is done. Child %d slept for %dsec\n", getpid() , child_1, child_1_num);
+
+        child_2 = fork();
+
+        if (child_2 < 0){
+            perror("child_2 fork fail\n");
+            exit(1);
+        }
+
+        if (child_2 == 0) {
+            printf("%d %dsec\n", getpid() ,child_2_num);
+            sleep(child_2_num);
+            printf("%d finished after %d seconds\n",getpid(), child_2_num);
+        } else {
+            int status;
+            pid_t child_pid = wait(&status);
+
+            if (child_pid == -1) {
+                perror("child_2 wait failed\n");
+                exit(1);
+            }
+
+            if (!(WIFEXITED(status))) {
+                printf("%d child_2 did not exit normally\n", getpid());
+            }
+
+            printf("Main Process %d is done. Child %d slept for %dsec\n", getpid() , child_2, child_2_num);
+        }
+
     }
-
-    pid_t child_2 = fork();
-    if (child_2 < 0){
-        perror("child_1 fork fail\n");
-        exit(1);
-    }
-    if (child_2 == 0) {
-        int rand_num = random_number();
-        printf("%d %dsec\n", getpid() ,rand_num);
-        sleep(rand_num);
-    }
-
-    int status;
-    pid_t child_pid = wait(&status);
-
-    if (!(WIFEXITED(status))) {
-        printf("%d child did not exit normally\n", getpid());
-    }
-
-    child_pid = wait(&status);
-    
-    // if (child_pid == -1) {
-    //     perror("child wait failed\n");
-    //     exit(1);
-    // }
-
-    if (!(WIFEXITED(status))) {
-        printf("%d child did not exit normally\n", getpid());
-    }
-
-    printf("parent done\n");
-    
 }
